f034d5c70ea84bee678549306d6c3b2daf86c07b
Ryan Triplett <triplettj4@mymail.shawnee.edu>
2026-03-20 02:46:10 -0400

    Added check for hit closeness

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -5,6 +5,7 @@ var BEAT = -1
 @onready var timer: Timer = self.find_child("Timer")
 var elapsed = 0.0
 var last_beat = -1
+var score: int = 0
 
 var song_position: float = 0.0
 
@@ -17,6 +18,13 @@ var track_actions: Dictionary[int, String] = {
 	24 : "right",
 	25 : "left",
 	26 : "right"
+}
+
+# Dictionary mapping closeness thresholds
+var closeness_threshold: Dictionary[String, float] = {	
+	"OK": 0.49, 
+	"Good": 0.25,
+	"Perfect": 0.15
 }
 
 # Called when the node enters the scene tree for the first time.
@@ -47,15 +55,45 @@ func _on_beat():
 		$MusicPlayer.play()
 		# track looping
 		if BEAT == 32: BEAT = 0
-	
-	# check for track actions
 
 func handle_input():
 	if Input.is_action_just_pressed("Left Hammer"):
 		$HammerSounds.pitch_scale = 0.8 + randf()/20
 		$HammerSounds.play(0.28)
 		print("Song Position (in beats): ", song_position)
+		if round(song_position) in track_actions.keys():
+			var closeness = abs(song_position - round(song_position))
+			check_hit(closeness, "left")
+			
 	if Input.is_action_just_pressed("Right Hammer"):
 		$HammerSounds.pitch_scale = 1.2 + randf()/20
 		$HammerSounds.play(0.28)
 		print("Song Position (in beats): ", song_position)
+		if round(song_position) in track_actions.keys():
+			var closeness = abs(song_position - round(song_position))
+			check_hit(closeness, "right")
+			
+# Calculate how close player is to the beat
+func check_hit(closeness, side):
+		# Round to closest whole number beat
+			# Check which threshold it is between
+			# Logic:
+				# 	If less than OK, but not less than Good, then it is OK
+				#	If less than Good, but not less than Perfect, then it is Good
+				#	If less than Perfect, then Perfect
+				
+			# TODO: What if they miss? More than 0.49 seconds but then it would go to next, potential bug? 
+			# Maybe track the most recent one they needed to hit, if that one is not hit, don't check it as round(song_position)???
+			# Red forman
+			if closeness <= closeness_threshold["OK"] && not closeness <= closeness_threshold["Good"]:
+				score += 100
+				print("OK: ", closeness, " from the time. On the ", side.to_upper(), " side")
+				print(score)
+			elif closeness <= closeness_threshold["Good"] && not closeness <= closeness_threshold["Perfect"]:
+				score += 250
+				print("Good: ", closeness, " from the time. On the ", side.to_upper(), " side")
+				print(score)
+			elif closeness <= closeness_threshold["Perfect"]:
+				score += 500
+				print("Perfect: ", closeness, "from the time. On the ", side.to_upper(), " side")
+				print(score)
