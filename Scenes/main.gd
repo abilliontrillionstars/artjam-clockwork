7c62d9bd463054089964ac4a09de29f9e2d04076
Laniakea <billiontrillionstars@gmail.com>
2026-03-19 23:45:19 -0400

    pink ponk pink (start hammer logic)

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -4,20 +4,31 @@ var BPM = 130 # aid in music syncing
 var BEAT = -1
 @onready var timer: Timer = self.find_child("Timer")
 var elapsed = 0.0
+var last_beat = -1
 
-@onready var placeholder_music = preload("res://Assets/music/start-loop-placeholder.wav")
+var track_actions: Dictionary[int, String] = {
+	16 : "left",
+	17 : "left",
+	18 : "right",
+	
+	24 : "right",
+	25 : "left",
+	26 : "right"
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	timer.start(1.0/(BPM/60.0))
-	timer.timeout.connect(_on_timeout)
+	timer.timeout.connect(_on_beat)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	elapsed += delta
+	handle_input()
 
-func _on_timeout():
+func _on_beat():
 	BEAT += 1
+	last_beat = elapsed
 	# play metronome
 	if BEAT%4 == 0: $AudioStreamPlayer.volume_db = -15
 	else: $AudioStreamPlayer.volume_db = -30
@@ -26,3 +37,17 @@ func _on_timeout():
 	if BEAT%32 == 0:
 		$MusicPlayer.stop()
 		$MusicPlayer.play()
+		# track looping
+		if BEAT == 32: BEAT = 0
+	
+	# check for track actions
+
+func handle_input():
+	if Input.is_action_just_pressed("Left Hammer"):
+		$HammerSounds.pitch_scale = 0.8 + randf()/20
+		$HammerSounds.play(0.28)
+		print("left: ", elapsed - last_beat + BEAT)
+	if Input.is_action_just_pressed("Right Hammer"):
+		$HammerSounds.pitch_scale = 1.2 + randf()/20
+		$HammerSounds.play(0.28)
+		print("right: ", elapsed - last_beat + BEAT)
