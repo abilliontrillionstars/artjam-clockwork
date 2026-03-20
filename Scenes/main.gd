a83a064307c31b59f63f62f4b1426aa045f395bf
Laniakea <billiontrillionstars@gmail.com>
2026-03-19 22:25:47 -0400

    sync music to beat (mostly)

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-var BPM = 30
-var BEAT = 0
+var BPM = 130 # aid in music syncing
+var BEAT = -1
 @onready var timer: Timer = self.find_child("Timer")
-
 var elapsed = 0.0
+
+@onready var placeholder_music = preload("res://Assets/music/start-loop-placeholder.wav")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,7 +17,12 @@ func _process(delta: float) -> void:
 	elapsed += delta
 
 func _on_timeout():
-	print("tick ", elapsed)
 	BEAT += 1
+	# play metronome
+	if BEAT%4 == 0: $AudioStreamPlayer.volume_db = -15
+	else: $AudioStreamPlayer.volume_db = -30
 	$AudioStreamPlayer.play()
-	
+	# play music loop
+	if BEAT%32 == 0:
+		$MusicPlayer.stop()
+		$MusicPlayer.play()
